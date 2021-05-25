8a1d6a2507d528063806ce0c5623cdb018281ac7
andreylr <andrey.rumyantsev@quantori.com>
2021-05-25 09:03:06 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,47 @@
+import pika
+import uuid
+
+class ReclalaJMClient(object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, command):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='reclada_rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=command)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+if __name__ == "__main__":
+    jm_client = ReclalaJMClient()
+    while True:
+        rpc_command = input("jm@")
+        response = jm_client.call(rpc_command)
+        response = response.decode("utf-8")
+        output = response
+        print(f"{response}")
---
+++ server.py
@@ -0,0 +1,44 @@
+import pika
+import subprocess
+
+
+class RecladaJMServer():
+    def __init__(self):
+        self._connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(queue='reclada_rpc_queue')
+        self._channel.basic_qos(prefetch_count=1)
+        self._channel.basic_consume(queue='reclada_rpc_queue', on_message_callback=self.on_request)
+
+    def handle_request(self, body):
+        body = body.decode("utf-8")
+        print(f'request: {body}')
+        try:
+            self._process = subprocess.run(body, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        except Exception as ex:
+            response = format(ex)
+            return response
+
+        output = self._process.stdout
+        error = self._process.stderr
+        response = error if error else output
+        response = f"{response.decode('utf-8')}"
+        return response
+
+    def on_request(self, ch, method, props, body):
+        response = self.handle_request(body)
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id= \
+                                                         props.correlation_id),
+                         body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def start(self):
+        self._channel.start_consuming()
+
+
+reclada_jm = RecladaJMServer()
+print(" [x] Awaiting requests")
+reclada_jm.start()
